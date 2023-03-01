70a3201411e6bb43b8e0ec42464c9b4164753a62
Bulat Sitdikov <b.sitdikov@wptt.ru>
2023-03-01 17:59:26 +0300

    add main menu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,104 @@
-const eventsSwiper = new Swiper(".events", {
-	slidesPerView: "auto",
-	spaceBetween: 0,
-});
+import takeControlAccordion from './accordion'
+
+const eventsSwiper = new Swiper('.events', {
+	slidesPerView: 'auto',
+	spaceBetween: 0
+})
+
+// search menu
+const searchBtn = document.getElementById('search-btn')
+const searchMenu = document.querySelector('.search-menu')
+searchBtn.addEventListener('click', () => {
+	searchMenu.classList.add('search-menu_active')
+})
+
+// burger menu
+const burgerBtn = document.getElementById('burger-btn')
+const menu = document.querySelector('.menu')
+burgerBtn.addEventListener('click', () => {
+	menu.classList.add('menu_active')
+})
+
+// bottom menu
+const VR = document.getElementById('virtual-reality')
+const vrMenu = document.getElementById('vr-menu')
+const RL = document.getElementById('remote-learning')
+const rlMenu = document.getElementById('rl-menu')
+const bottomSearchButton = document.getElementById('bottom-search-btn')
+const bottomBurgerButton = document.getElementById('bottom-burger-btn')
+
+VR.addEventListener('click', () => {
+	rlMenu.classList.remove('service-menu_active')
+	searchMenu.classList.remove('search-menu_active')
+	menu.classList.remove('menu_active')
+	vrMenu.classList.toggle('service-menu_active')
+})
+
+RL.addEventListener('click', () => {
+	vrMenu.classList.remove('service-menu_active')
+	searchMenu.classList.remove('search-menu_active')
+	menu.classList.remove('menu_active')
+	rlMenu.classList.toggle('service-menu_active')
+})
+
+bottomSearchButton.addEventListener('click', () => {
+	rlMenu.classList.remove('service-menu_active')
+	vrMenu.classList.remove('service-menu_active')
+	menu.classList.remove('menu_active')
+	searchMenu.classList.toggle('search-menu_active')
+})
+
+bottomBurgerButton.addEventListener('click', () => {
+	rlMenu.classList.remove('service-menu_active')
+	vrMenu.classList.remove('service-menu_active')
+	searchMenu.classList.remove('search-menu_active')
+	menu.classList.toggle('menu_active')
+})
+
+// close all menu
+const menuCloseButtons = document.querySelectorAll('.close-btn')
+menuCloseButtons.forEach((elem) => {
+	elem.addEventListener('click', () => {
+		rlMenu.classList.remove('service-menu_active')
+		vrMenu.classList.remove('service-menu_active')
+		menu.classList.remove('menu_active')
+		searchMenu.classList.remove('search-menu_active')
+	})
+})
+
+// dropdown select
+const elSelect = document.getElementsByClassName('select')[0]
+const elSelectValue = elSelect.children[0]
+const elSelectOptions = elSelect.children[1]
+
+Array.from(elSelectOptions.children).forEach((elOption) => {
+	elOption.addEventListener('click', (e) => {
+		elSelectValue.textContent = e.target.textContent
+		elSelect.classList.remove('isActive')
+	})
+})
+
+elSelectValue.addEventListener('click', () => {
+	elSelect.classList.toggle('isActive')
+})
+
+document.addEventListener('click', (e) => {
+	const didClickedOutside = !elSelect.contains(e.target)
+	if (didClickedOutside) {
+		elSelect.classList.remove('isActive')
+	}
+})
+
+// menu accordeon
+takeControlAccordion('.menu__accordion', '.accordion__list', {
+	accordionItemSelector: '.accordion__item',
+	accordionItemTogglerSelector: '.accordion__toggle',
+	accordionItemContentSelector: '.accordion__content',
+	accordionItemActiveClass: 'accordion__item_active'
+})
+
+const menuToggle = document.querySelector('.accordion__toggle')
+const subMenu = document.querySelector('.accordion__content')
+menuToggle.addEventListener('mouseover', () => {
+	subMenu.classList.add('accordion__content_active')
+})
