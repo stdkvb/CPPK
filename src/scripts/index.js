c6ca6b24172519cb3103c37c0acdf774f64df646
Bulat Sitdikov <b.sitdikov@wptt.ru>
2023-03-02 17:58:08 +0300

    add Benefits

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -138,3 +138,26 @@ const subMenu = document.querySelector('.accordion__content')
 menuToggle.addEventListener('mouseover', () => {
 	subMenu.classList.add('accordion__content_active')
 })
+
+// benefits backgrounds
+const benefitsCards = document.querySelector('.benefits__cards')
+benefitsCards.children[0].addEventListener('mouseover', (e) => {
+	benefitsCards.style.backgroundImage = 'url(../files/images/benefits-background-1.png)'
+	e.target.classList.add('benefits__card_active')
+})
+benefitsCards.children[1].addEventListener('mouseover', (e) => {
+	benefitsCards.style.backgroundImage = 'url(../files/images/benefits-background-2.png)'
+	e.target.classList.add('benefits__card_active')
+})
+benefitsCards.children[2].addEventListener('mouseover', (e) => {
+	benefitsCards.style.backgroundImage = 'url(../files/images/benefits-background-3.png)'
+	e.target.classList.add('benefits__card_active')
+})
+benefitsCards.children[3].addEventListener('mouseover', (e) => {
+	benefitsCards.style.backgroundImage = 'url(../files/images/benefits-background-4.png)'
+	e.target.classList.add('benefits__card_active')
+})
+benefitsCards.children[4].addEventListener('mouseover', (e) => {
+	benefitsCards.style.backgroundImage = 'url(../files/images/benefits-background-5.png)'
+	e.target.classList.add('benefits__card_active')
+})
