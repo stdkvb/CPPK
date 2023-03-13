d7f5442e8b1d26b3a793c9b6e419b3bf94e0cf71
Bulat Sitdikov <b.sitdikov@wptt.ru>
2023-03-13 18:00:50 +0300

    add select component

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -135,7 +135,7 @@ const navBarSwiper = new Swiper('.nav-bar', {
 	spaceBetween: 44,
 	breakpoints: {
 		992: {
-			spaceBetween: 80,
+			spaceBetween: 80
 		}
 	}
 })
@@ -350,24 +350,142 @@ takeControlAccordion('.direction__filters', '.accordion__list', {
 })
 
 // Btn "Show-more"
-const clientsElements = document.querySelector('.clients');
-const moreButton = document.querySelector('.sub-wrapper__more > .btn_primary_blue');
+const clientsElements = document.querySelector('.clients')
+const moreButton = document.querySelector('.sub-wrapper__more > .btn_primary_blue')
 if (moreButton !== null) {
-	moreButton = addEventListener("click", function () {
-		clientsElements.classList.add('active');
+	moreButton = addEventListener('click', () => {
+		clientsElements.classList.add('active')
 	})
 }
 
 // Video
-const playButton = document.querySelector('.video-controler');
-const videoContainer = document.querySelector('.video-container video');
+const playButton = document.querySelector('.video-controler')
+const videoContainer = document.querySelector('.video-container video')
 if (videoContainer !== null) {
 	playButton.addEventListener('click', () => {
-		playButton.classList.add('active');
-		videoContainer.play();
+		playButton.classList.add('active')
+		videoContainer.play()
 	})
 	videoContainer.addEventListener('click', () => {
-		playButton.classList.remove('active');
-		videoContainer.pause();
+		playButton.classList.remove('active')
+		videoContainer.pause()
 	})
 }
+
+function customSelect(id) {
+	let x
+	let i
+	let j
+	let l
+	let closeArrow
+	let ll
+	let selElmnt
+	let a
+	let b
+	let arrow
+	let c
+	/* look for any elements with the class "custom-select": */
+	x = document.getElementsByClassName(id)
+	l = x.length
+
+	closeArrow = document.getElementById('select01')
+
+	for (i = 0; i < l; i++) {
+		selElmnt = x[i].getElementsByTagName('select')[0]
+		ll = selElmnt.length
+		/* for each element, create a new DIV that will act as the selected item: */
+		a = document.createElement('DIV')
+		a.setAttribute('class', 'select-selected')
+		a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML
+		x[i].appendChild(a)
+
+		/* for each element, create a new DIV that will contain the option list: */
+		b = document.createElement('DIV')
+		b.setAttribute('class', 'select-items select-hide')
+
+		for (j = 1; j < ll; j++) {
+			/* for each option in the original select element,
+    create a new DIV that will act as an option item: */
+			c = document.createElement('DIV')
+			c.innerHTML = selElmnt.options[j].innerHTML
+			c.addEventListener('click', function (e) {
+				/* when an item is clicked, update the original select box,
+        and the selected item: */
+				let y
+				let i
+				let k
+				let s
+				let h
+				let sl
+				let yl
+				s = this.parentNode.parentNode.getElementsByTagName('select')[0]
+				sl = s.length
+				h = this.parentNode.previousSibling
+
+				for (i = 0; i < sl; i++) {
+					if (s.options[i].innerHTML == this.innerHTML) {
+						s.selectedIndex = i
+						h.innerHTML = this.innerHTML
+						y = this.parentNode.getElementsByClassName('selected-item')
+						yl = y.length
+
+						for (k = 0; k < yl; k++) {
+							y[k].removeAttribute('class')
+						}
+						this.setAttribute('class', 'selected-item')
+						break
+					}
+				}
+				h.click()
+			})
+			b.appendChild(c)
+		}
+
+		x[i].appendChild(b)
+		a.addEventListener('click', function (e) {
+			/* when the select box is clicked, close any other select boxes,
+      and open/close the current select box: */
+			e.stopPropagation()
+			closeAllSelect(this)
+			this.nextSibling.classList.toggle('select-hide')
+			closeArrow.classList.toggle('open')
+			this.classList.toggle('select-arrow-active')
+		})
+	}
+
+	function closeAllSelect(elmnt) {
+		/* a function that will close all select boxes in the document,
+  except the current select box: */
+		let x
+		let y
+		let i
+		let xl
+		let yl
+		const arrNo = []
+		x = document.getElementsByClassName('select-items')
+		y = document.getElementsByClassName('select-selected')
+		xl = x.length
+		yl = y.length
+
+		for (i = 0; i < yl; i++) {
+			if (elmnt == y[i]) {
+				arrNo.push(i)
+			} else {
+				y[i].classList.remove('select-arrow-active')
+			}
+		}
+
+		for (i = 0; i < xl; i++) {
+			if (arrNo.indexOf(i)) {
+				x[i].classList.add('select-hide')
+				closeArrow.classList.remove('open')
+				// x[i]closeArrow.classList.add("open");
+			}
+		}
+	}
+	/* if the user clicks anywhere outside the select box,
+then close all select boxes: */
+	document.addEventListener('click', closeAllSelect)
+}
+
+customSelect('question01')
