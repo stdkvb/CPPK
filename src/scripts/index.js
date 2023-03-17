d772337593189c2a0b90dc875cd1ed54f5f59953
Bulat Sitdikov <b.sitdikov@wptt.ru>
2023-03-17 11:22:25 +0300

    add left-bar animation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -518,3 +518,29 @@ then close all select boxes: */
 }
 
 customSelect('custom-select')
+
+// left bar scroll animation
+const leftBar = document.querySelector('.left-bar')
+const programmsSection = document.querySelector('.programms')
+
+const visible = function (target) {
+	const targetPosition = {
+		top: window.pageYOffset + target.getBoundingClientRect().top
+	}
+	const windowPosition = {
+		top: window.pageYOffset,
+		left: window.pageXOffset,
+		right: window.pageXOffset + document.documentElement.clientWidth,
+		bottom: window.pageYOffset + document.documentElement.clientHeight
+	}
+
+	if (targetPosition.top < windowPosition.bottom) {
+		leftBar.classList.remove('left-bar_transparent')
+	} else {
+		leftBar.classList.add('left-bar_transparent')
+	}
+}
+window.addEventListener('scroll', () => {
+	visible(programmsSection)
+})
+visible(programmsSection)
